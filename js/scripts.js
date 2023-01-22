2de176c405089ad74ca1a1df7448fca9cc208512
jula <122871245+jmahanera@users.noreply.github.com>
2023-01-22 13:09:17 +0100

    Update scripts.js
    updated variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
-let pokemonlist = [ 'Ivysaur', 'charmeleon', 'venasaur'],
-name = 'Bulbasaur',
-height = 270,
-types = [ 'grass', 'sand', 'water'];
+let pokemonlist = [ 
+    {name: 'Balbasaur',
+    height: 12,
+    types: 'grass', 'sand': 'poison'},
+    {name: 'Exeggutor',
+    height: 18, 
+    types:  'grass', 'sand': 'poison'},
+    {name: 'lickitung',
+    height: 16, 
+    types: 'grass', 'sand': 'poison'}
+];
+
+
 
 
