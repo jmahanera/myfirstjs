d17b4f74cd644dd7c8aef6c49f7796f205adef10
jula <122871245+jmahanera@users.noreply.github.com>
2023-01-24 23:50:33 +0100

    updated pokemonlist
    Pokemonlist with objects and heights added using console.log instead of document.write

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,35 @@ function add(pokemon) {
     return pokemonList;
   }
 
+  let pokeMonlist =[
+    {name: 'Balbasaur', height: 12, types: ['grass', 'poison', 'odor']},
+    {name: 'Charmander', height: 11, types:  ['gass','fire', 'wings']},
+    {name: 'Squirtle', height: 12, types: ['slow', 'hard', 'shell']},
+];
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  
+  function getAll() {
+    return pokemonList;
+  }
+
+  /* The for loop using console.log instead of document write because document write
+  gives me an error due to the slow speed of my internet it said. The console list loop 
+  below worked perfectly.
+
+  for (let i = 0; i < pokemonList.length; i++) {        
+    if (pokemonList[i].height >= 5) {
+      console.log(pokemonList[i].name + " (height: " + pokemonList[i].height );
+    } else if (pokemonList[i].height >= 1.6 && pokemonList[i].height < 5) {
+      console.log(pokemonList[i].name + " (height: " +pokemonList[i].height );
+    } else {
+      console.log(pokemonList[i].name + " (height: " + pokemonList[i].height);
+    }
+  }
+  */
+
   // The for loop
 for (let i = 0; i < pokemonList.length; i++) {        
     if (pokemonList[i].height >= 5) {
