891703345ea341ce72691a4877ee3fbefc7e0ee6
jula <122871245+jmahanera@users.noreply.github.com>
2023-01-25 00:20:20 +0100

    Updated
    Changed let pekemonlist declaration to let repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
     
     
-    /*let pokemonList =[
+    let repository =[
     {name: 'Balbasaur', height: 12, types: ['grass', 'poison', 'odor']},
     {name: 'Charmander', height: 11, types:  ['gass','fire', 'wings']},
     {name: 'Squirtle', height: 12, types: ['slow', 'hard', 'shell']},
-];*/
+];
 
+function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
   
  
 
