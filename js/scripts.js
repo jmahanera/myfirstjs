4c898f29f1b96824c926206a67911ac61f3b7da2
jula <122871245+jmahanera@users.noreply.github.com>
2023-01-25 00:12:51 +0100

    Update scripts.js
    Double declaration commented

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
     
     
-    let pokemonList =[
+    /*let pokemonList =[
     {name: 'Balbasaur', height: 12, types: ['grass', 'poison', 'odor']},
     {name: 'Charmander', height: 11, types:  ['gass','fire', 'wings']},
     {name: 'Squirtle', height: 12, types: ['slow', 'hard', 'shell']},
-];
+];*/
 
-function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
   
  
 
