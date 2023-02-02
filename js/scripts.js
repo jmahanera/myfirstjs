89f6dde73dbc38b21b65458b4236a5d27eb71b7e
jula <122871245+jmahanera@users.noreply.github.com>
2023-02-02 22:44:00 +0100

    deleted
    Deleted unecessary comments in JS

--- js/scripts.js
+++ js/scripts.js
@@ -23,25 +23,7 @@
       add: add
   }
 })()
-      
-     
-    
-      /* The for loop using console.log instead of document write because document.write
-      was giving me an error due to the slow speed of my internet it said. The console list loop 
-      below worked perfectly.*/
-    
-      /*for (let i = 0; i < pokemonList.length; i++) {        
-        if (pokemonList[i].height >= 5) {
-          console.log(pokemonList[i].name + " (height: " + pokemonList[i].height );
-        } else if (pokemonList[i].height >= 1.6 && pokemonList[i].height < 5) {
-          console.log(pokemonList[i].name + " (height: " +pokemonList[i].height );
-        } else {
-          console.log(pokemonList[i].name + " (height: " + pokemonList[i].height);
-        }
-      }*/
-      
-    
-      // The for loop using document.write.
+   // The for loop using document.write.
     /*for (let i = 0; i < pokemonList.length; i++) {        
         if (pokemonList[i].height >= 5) {
           document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - Wow, that is a big pokemon!");
