7fc37dab16ea29e607b0c36cef4f686712b31faf
jula <122871245+jmahanera@users.noreply.github.com>
2023-02-03 09:57:09 +0100

    foeEach rectified
    forEach method adopted and rectified to make sure the code syntax works

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
     
-    let pokemonRepository = (function () {
     
     let pokemonList =[
         {name: 'Balbasaur', height: 12, types: ['grass', 'poison']},
@@ -8,21 +7,32 @@
       }
 
     ]
+
     
+    let pokemonRepository = (function () {
+    
+      let pokemonList =[
+          {name: 'Balbasaur', height: 12, types: ['grass', 'poison']},
+          {name: 'Charmander', height: 2.5, types:  ['fire', 'wings']},
+          {name: 'Squirtle', height: 1, types: ['slow', 'shell']
+        }
+  
+      ]
+
+
+    function add (pokemon) {
+      pokemonList.push(pokemon);
+    }
     
     function getAll () {
       return pokemonList;
     }
 
-    function add (pokemon) {
-      pokemonList.push(pokemon);
-    }
-
     return {
-      getAll: getAll,
-      add: add
-  }
-})()
+      add: add,
+      getAll: getAll
+        };
+})();
    // The for loop using document.write.
     /*for (let i = 0; i < pokemonList.length; i++) {        
         if (pokemonList[i].height >= 5) {
@@ -34,9 +44,9 @@
         }
       }*/
 
-      pokemonList.forEach(function(pokemon){
+      pokemonRepository.getAll().forEach(function(pokemon){
         //console.log(pokemon.name + 'is' + pokemon.height + pokemon.types );
-        console.log(pokemonRepository.getAll())
+        document.write(pokemon.name + pokemon.height + pokemon.types)
       });
       
      
