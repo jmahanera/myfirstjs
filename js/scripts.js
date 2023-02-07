6fe1dea58fbb1894310682e2ba990bfe1ec11b2e
jula <122871245+jmahanera@users.noreply.github.com>
2023-02-07 12:01:23 +0100

    Update scripts.js
    append the button to the list item as its child

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,8 @@
         let button = document.createElement('button');
         button.innerText = 'Charmander';
         button.classList.add('selected');
-        pokemonList.appendChild(button);
+        listItem.appendChild(button);
+        ul.appendChild(listItem);
       });
       
      
